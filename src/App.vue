09734e9bd4558fb51a80f5616cc17798bac0d204
wins <wins@hjtech.tw>
2022-11-24 15:20:53 +0800

    feat: base logic

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,5 @@ const mode = ref<Mode>(TransformModeList[0])
       />
     </section>
     <router-view />
-    <button class=" bg-orange-300 hover:bg-orange-400 text-xl rounded-lg border-2 border-black block w-full py-4 font-bold">
-      convert
-    </button>
   </Layout>
 </template>
--- src/pages/ExcelToJson.vue
+++ src/pages/ExcelToJson.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import type { WorkBook } from 'xlsx'
 import { read, utils } from 'xlsx'
 import { saveAs } from 'file-saver'
 import { ref, vModelCheckbox } from 'vue'
@@ -15,29 +16,69 @@ const path = ref('')
 const chooseFileName = ref('')
 const outputFolderPath = ref('')
 
-async function handleFile() {
-  const [fileHandler] = await window.showOpenFilePicker()
-  chooseFileName.value = fileHandler.name
+async function openFile() {
+  try {
+    const [fileHandler] = await window.showOpenFilePicker()
 
-  const file = await fileHandler.getFile()
-  const source = await file.text()
-  // const workBook = read(source)
-  // console.log(workBook)
-  // console.log(source)
+    const file = await fileHandler.getFile()
+    const data = await file.arrayBuffer()
+    const wb = read(data)
 
-  // const target = e.target! as HTMLInputElement
-  // const fileList = target.files
-  // if (!fileList)
-  //   return
+    if (wb.bookType !== 'xlsx')
+      return alert('this file is not xlsx!! please select xlsx format file')
 
-  // chooseFileName.value = fileList[0].name
+    chooseFileName.value = fileHandler.name
+    const canParse = checkSheetsHaveKeyField(wb)
+    if (!canParse)
+      return
+  }
+  catch (error) {
+    console.error((error as Error).message)
+  }
+}
 
-  // const source = await fileList[0].arrayBuffer()
+function checkSheetsHaveKeyField(workBook: WorkBook) {
+  const missKeySheetNames = []
 
-  // /* parse and load first worksheet */
-  // const wb = read(source)
-  // const data = utils.sheet_to_json<President>(wb.Sheets[wb.SheetNames[0]])
-  // outputJsonData.value = data
+  for (let a = 0; a < Object.values(workBook.Sheets).length; a++) {
+    const sheet = Object.values(workBook.Sheets)[a]
+    /** e.g. [A1, A2, A3, B1, B2, B3, C1, C2] */
+    const positionList = Object.keys(sheet)
+    /** e.g. [A1, B1, C1, D1] */
+    const headList = positionList.filter(position => position.match(/^[a-zA-Z]1$/))
+    const hasKey = headList.find((head) => {
+      return (sheet[head].v as string).trim().toLowerCase() === 'key'
+    })
+
+    if (!hasKey) {
+      const currentSheetName = workBook.SheetNames[a]
+      missKeySheetNames.push(currentSheetName)
+    }
+  }
+
+  if (missKeySheetNames.length) {
+    const message = missKeySheetNames.reduce((prev, curr) => {
+      prev += `${curr}\n`
+      return prev
+    }, 'those sheet doesn\'t have key on header: \n-------------\n')
+    alert(message)
+    return false
+  }
+
+  return true
+}
+
+function checkSheetsHaveAllLang(workBook: WorkBook) {
+
+}
+
+function convert() {
+  if (!chooseFileName.value || !outputFolderPath.value) {
+    alert('required select file and target folder')
+    return
+  }
+
+  console.log('convert ite')
 }
 
 function exportJsonFile() {
@@ -71,23 +112,23 @@ async function handleDirectory() {
       <button
         class="inline-block p-4 font-mono text-sm font-bold text-gray-900 border-2 border-black rounded-l-lg cursor-pointer focus:outline-none bg-slate-300 hover:bg-slate-700 hover:text-white"
         type="button"
-        @click="handleFile"
+        @click="openFile"
       >
         select file
       </button>
-      <div class="flex-1 px-4 border-2 border-l-0 border-black rounded-r-lg flex items-center">
+      <div class="flex items-center flex-1 px-4 border-2 border-l-0 border-black rounded-r-lg">
         {{ chooseFileName }}
       </div>
     </div>
 
     <!-- select target folder -->
-    <h4 class="mb-2 text-2xl mt-8">
+    <h4 class="mt-8 mb-2 text-2xl">
       Output folder
     </h4>
     <div class="pl-4 mb-3 border-l-2 border-black bg-slate-100">
       output file will export to this folder
     </div>
-    <div class="flex">
+    <div class="flex mb-8">
       <button
         class="inline-block p-4 font-mono text-sm font-bold text-gray-900 border-2 border-black rounded-l-lg cursor-pointer focus:outline-none bg-slate-300 hover:bg-slate-700 hover:text-white"
         type="button"
@@ -95,10 +136,16 @@ async function handleDirectory() {
       >
         folder path
       </button>
-      <div class="flex-1 px-4 border-2 border-l-0 border-black rounded-r-lg flex items-center">
-        {{ chooseFileName }}
+      <div class="flex items-center flex-1 px-4 border-2 border-l-0 border-black rounded-r-lg">
+        {{ outputFolderPath }}
       </div>
     </div>
+    <button
+      class="block w-full py-4 text-xl font-bold bg-orange-300 border-2 border-black rounded-lg active:bg-orange-500 hover:bg-orange-400"
+      @click="convert"
+    >
+      convert
+    </button>
     <!-- // 後續看要不要補錯誤訊息
     <div class="absolute mt-1 ml-2 text-red-500" /> -->
   </section>
